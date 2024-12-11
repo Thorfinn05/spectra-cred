4a43735b35f99941ed5b68dd289440d82e919682
Anirban Ghosh <anirbanghosh060@gmail.com>
2024-12-11 18:30:39 +0530

    added .env example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 
 
 export default async function Page() {
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (!userId) {
     redirect("/dashboard");
